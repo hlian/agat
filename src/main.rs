390ea9be9332af2131897827cbd8ce0f28546372
Hao Lian <hi@haolian.org>
2019-05-28 19:20:41 -0400

    main: etags!

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use failure::{bail, err_msg, Error};
 use github_rs::client::{Executor, Github};
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
-    login: String
+    login: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -18,20 +17,46 @@ struct PR {
     created_at: DateTime<Utc>,
 }
 
-fn main2(token: &str) -> Result<(), Error> {
+#[derive(Debug)]
+struct Cache<T> {
+    value: T,
+    etag: http::header::HeaderValue,
+}
+
+fn prs(token: &str, cache: Option<Cache<Vec<PR>>>) -> Result<Cache<Vec<PR>>, Error> {
     let client = Github::new(token).map_err(failure::SyncFailure::new)?;
-    let (_headers, status, json) = client
-        .get()
+    let builder = client.get();
+    let builder2 = match &cache {
+        Some(Cache { etag, .. }) => builder.set_etag(etag),
+        None => builder,
+    };
+
+    let (headers, status, json) = builder2
         .repos()
         .owner("Originate")
         .repo("dashboard")
         .pulls()
         .execute::<Vec<PR>>()
         .map_err(failure::SyncFailure::new)?;
-    if status != 200 {
+    if status == 304 {
+        Ok(cache.ok_or_else(|| err_msg("impossible"))?)
+    } else if status != 200 {
         bail!("nope {}", status);
+    } else {
+        println!("{:?}", headers);
+        Ok(Cache {
+            value: json.ok_or_else(|| err_msg("invalid response"))?,
+            etag: github_rs::headers::etag(&headers)
+                .ok_or_else(|| err_msg("missing etag"))?
+                .clone(),
+        })
     }
-    println!("{:?}", &json.ok_or_else(|| err_msg("bummer"))?);
+}
+
+fn main2(token: &str) -> Result<(), Error> {
+    let cache = prs(token, None)?;
+    let etag2 = prs(token, Some(cache))?;
+    println!("success {:?}, {}", etag2.value, etag2.etag.to_str()?);
     Ok(())
 }
 
